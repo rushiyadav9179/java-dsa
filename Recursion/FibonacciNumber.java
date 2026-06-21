eb70a61883aaec2003acf726f567b4cece61e1b6
Rushikesh Yadav <rushikeshy545@gmail.com>
2026-06-21 21:50:53 +0530

    Solved Fibonacci Number using iterative DP

--- Recursion/FibonacciNumber.java
+++ Recursion/FibonacciNumber.java
@@ -39,43 +39,4 @@ public class FibonacciNumber {
 
         System.out.println("Fibonacci Number: " + result);
     }
-}// Problem Name: Fibonacci Number
-// Topic: Dynamic Programming, Math
-// Time Complexity: O(n)
-// Space Complexity: O(1)
-
-public class FibonacciNumber {
-
-    public int solution(int n) {
-
-        if(n <= 1) {
-            return n;
-        }
-
-        int pre = 0;
-        int first = 1;
-        int curr;
-
-        for(int i = 1; i < n; i++) {
-
-            curr = pre + first;
-
-            pre = first;
-
-            first = curr;
-        }
-
-        return first;
-    }
-
-    public static void main(String[] args) {
-
-        FibonacciNumber obj = new FibonacciNumber();
-
-        int n = 6;
-
-        int result = obj.solution(n);
-
-        System.out.println("Fibonacci Number: " + result);
-    }
 }
