664813542d5c28547e9f5717f6f25cae9b26f2b9
Rushikesh Yadav <rushikeshy545@gmail.com>
2026-07-02 23:29:45 +0530

    Solved Valid Parentheses using stack

---
+++ Stack/ValidParentheses.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+import java.util.Stack;
+// Problem Name: Valid Parentheses
+// Topic: Stack, HashMap
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+public class ValidParentheses {
+
+    public boolean solution(String s) {
+
+        HashMap<Character, Character> map = new HashMap<>();
+
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+
+            if(map.containsKey(c)) {
+
+                stack.push(c);
+
+            } else {
+
+                if(stack.isEmpty()) {
+                    return false;
+                }
+
+                char t = stack.pop();
+
+                if(c != map.get(t)) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        ValidParentheses obj = new ValidParentheses();
+
+        String s = "()[]{}";
+
+        boolean result = obj.solution(s);
+
+        System.out.println("Is Valid: " + result);
+    }
+}
