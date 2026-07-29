e14d092adc2b4b8960fb12c1aadd92095e23ada4
Rushikesh Yadav <rushikeshy545@gmail.com>
2026-07-29 21:28:43 +0530

    Solved Kth Largest Element in an Array using Sorting

--- Sorting/KthLargestElementInArray.java
+++ Sorting/KthLargestElementInArray.java
@@ -25,7 +25,7 @@ public class KthLargestElementInArray {
         int[] arr = {3, 2, 1, 5, 6, 4};
         int k = 2;
 
-        int result = obj.solution(arr);
+        int result = obj.solution(arr, k);
 
         System.out.println("Kth Largest Element: " + result);
     }
