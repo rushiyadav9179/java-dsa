02d446e8bc18f38f62d3628b4c36ef60da48bb51
Rushikesh Yadav <rushikeshy545@gmail.com>
2026-08-01 23:31:56 +0530

    Solved Height Checker using Sorting

---
+++ Arrays/HeightChecker.java
@@ -0,0 +1,45 @@
+package Arrays;
+
+import java.util.Arrays;
+
+// Problem Name: Height Checker
+// Topic: Arrays, Sorting
+// Time Complexity: O(n log n)
+// Space Complexity: O(n)
+
+public class HeightChecker {
+
+    public int solution(int[] heights) {
+
+        int n = heights.length;
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = heights[i];
+        }
+
+        Arrays.sort(arr);
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] != heights[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        HeightChecker obj = new HeightChecker();
+
+        int[] heights = {1, 1, 4, 2, 1, 3};
+
+        int result = obj.solution(heights);
+
+        System.out.println("Students Out of Place: " + result);
+    }
+}
