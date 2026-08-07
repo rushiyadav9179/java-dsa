e81fc338944522b7693ae512317e6613cfe93996
Rushikesh Yadav <rushikeshy545@gmail.com>
2026-08-07 23:53:02 +0530

    Solved Binary Tree Inorder Traversal using Recursion

---
+++ Trees/BinaryTreeInorderTraversal.java
@@ -0,0 +1,61 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Problem Name: Binary Tree Inorder Traversal
+// Topic: Trees, DFS, Recursion
+// Time Complexity: O(n)
+// Space Complexity: O(h)
+
+public class BinaryTreeInorderTraversal {
+
+    // Definition for a binary tree node.
+    static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    ArrayList<Integer> ans;
+
+    public void inorder(TreeNode node) {
+
+        if(node == null) {
+            return;
+        }
+
+        inorder(node.left);
+
+        ans.add(node.val);
+
+        inorder(node.right);
+    }
+
+    public List<Integer> solution(TreeNode root) {
+
+        ans = new ArrayList<>();
+
+        inorder(root);
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        BinaryTreeInorderTraversal obj = new BinaryTreeInorderTraversal();
+
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        List<Integer> result = obj.solution(root);
+
+        System.out.println(result);
+    }
+}
