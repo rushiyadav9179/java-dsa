a3a79ca0acf8d4ef07557ac411c10bbed65a093d
Rushikesh Yadav <rushikeshy545@gmail.com>
2026-08-15 22:42:01 +0530

    Update Maximum Depth of Binary Tree with Top Down DFS

--- Trees/Maximum_BottomUp.java
+++ Trees/Maximum_BottomUp.java
@@ -8,82 +8,48 @@ package Trees;
 
 public class Maximum_BottomUp {
 
-    // Top-Down DFS approach
-    public int depth(TreeNode node, int currentDepth) {
+    int ans;
+
+    // Top-Down DFS
+    public void depth(TreeNode node, int depth) {
 
         if(node == null) {
-            return currentDepth - 1;
+            return;
         }
 
-        int left = depth(node.left, currentDepth + 1);
-        int right = depth(node.right, currentDepth + 1);
+        ans = Math.max(ans, depth);
 
-        return Math.max(left, right);
+        depth(node.left, depth + 1);
+        depth(node.right, depth + 1);
     }
 
     public int solution(TreeNode root) {
+
+        ans = 0;
+
         if(root == null) {
             return 0;
         }
 
-        return depth(root, 1);
+        depth(root, 1);
+
+        return ans;
     }
 
+    public static void main(String[] args) {
 
-    /*
-     * Bottom-Up DFS approach
-     *
-     * public int depth(TreeNode node) {
-     *
-     *     if(node == null) {
-     *         return 0;
-     *     }
-     *
-     *     int left = depth(node.left);
-     *     int right = depth(node.right);
-     *
-     *     return 1 + Math.max(left, right);
-     * }
-     *
-     * public int solution(TreeNode root) {
-     *     return depth(root);
-     * }
-     */
+        Maximum_BottomUp obj = new Maximum_BottomUp();
 
+        TreeNode root = new TreeNode(3);
 
-    /*
-     * Iterative BFS approach
-     *
-     * public int solution(TreeNode root) {
-     *
-     *     if(root == null) {
-     *         return 0;
-     *     }
-     *
-     *     int count = 0;
-     *     Queue<TreeNode> q = new LinkedList<>();
-     *     q.add(root);
-     *
-     *     while(!q.isEmpty()) {
-     *
-     *         int size = q.size();
-     *         count++;
-     *
-     *         for(int i = 0; i < size; i++) {
-     *
-     *             TreeNode node = q.remove();
-     *
-     *             if(node.left != null) {
-     *                 q.add(node.left);
-     *             }
-     *
-     *             if(node.right != null) {
-     *                 q.add(node.right);
-     *             }
-     *         }
-     *     }
-     *
-     *     return count;
-     * }
-     */
+        root.left = new TreeNode(9);
+
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        int result = obj.solution(root);
+
+        System.out.println("Maximum Depth: " + result);
+    }
 }
