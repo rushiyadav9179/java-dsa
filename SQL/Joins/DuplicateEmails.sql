46efe51bddad92b1be0b67677445be763e1dad98
Rushikesh Yadav <rushikeshy545@gmail.com>
2026-08-19 23:57:53 +0530

    Solved Duplicate Emails SQL problem

---
+++ SQL/Joins/DuplicateEmails.sql
@@ -0,0 +1,10 @@
+-- Problem Name: Duplicate Emails
+-- Topic: Self JOIN, DISTINCT
+-- Difficulty: Easy
+-- LeetCode: 182
+
+SELECT DISTINCT p2.email AS Email
+FROM Person AS p1
+JOIN Person AS p2
+ON p1.email = p2.email
+AND p1.id < p2.id;
