418e86b711968316391f07cb9a7d6d0bf7077d0c
Rushikesh Yadav <rushikeshy545@gmail.com>
2026-08-31 02:04:18 +0530

    Solved Employees Earning More Than Their Managers

---
+++ SQL/Joins/ManagersWithAtLeast5DirectReports.sql
@@ -0,0 +1,8 @@
+-- Problem Name: Employees Earning More Than Their Managers
+-- Topic: SQL, Self Join
+
+SELECT e1.name AS Employee
+FROM Employee AS e1
+LEFT JOIN Employee AS e2
+ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary;
